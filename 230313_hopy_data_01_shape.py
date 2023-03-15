4c692b74520e96e0281c3deb5c53886095248570
Tiffany Han <125551867+tiffanyhansy@users.noreply.github.com>
2023-03-15 18:12:04 +0900

    Create 230313_hopy_data_01_shape.py

---
+++ 230313_hopy_data_01_shape.py
@@ -0,0 +1,15 @@
+#p15
+#np.array() 로우, 칼럼, 데이터 개수 알 수 있다.
+
+
+import numpy as np
+
+array1 = np.array([1,2,3])
+array2 = np.array([1,2,3,4,5,6,8])
+
+
+print('array1 type:', type(array1))
+print('array1 array 형태 :', array1.shape)
+
+print('array2 type:', type(array2))
+print('array2 array 형태 :', array2.shape)
