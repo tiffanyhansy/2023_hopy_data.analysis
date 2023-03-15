c0c9a329401fe627f6e4b9db3b52b78fe2dcb797
Tiffany Han <125551867+tiffanyhansy@users.noreply.github.com>
2023-03-15 18:12:56 +0900

    Create 230315_hopy_data_01_arange.py

---
+++ 230315_hopy_data_01_arange.py
@@ -0,0 +1,12 @@
+'''
+p19
+arange() : array 를 range 로 표현
+'''
+import numpy as np
+
+sequence_array = np.arange(10)
+print(sequence_array)
+print(sequence_array.dtype, sequence_array.shape)   #shape 로 데이터 모양 알 수 있
+
+
+
